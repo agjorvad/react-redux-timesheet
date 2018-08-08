c4e429b2e5658aeea895c2f677a1bea017a31f40
Alex Gjorvad <alexgjorvad@gmail.com>
2018-08-08 14:36:26 -0500

    integrating redux

--- lab-04-redux/src/components/employees/EmployeeRow.js
+++ lab-04-redux/src/components/employees/EmployeeRow.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
 
-class EmployeeRow extends Component {
-  render() {
-    const employee = this.props.employee;
+
+  class EmployeeRow extends Component {
+    handleClick(employee) {
+      if (employee.deleted) {
+        employee.deleted = false;
+        this.props.actions
+          .restoreEmployee(employee)
+          .then(this.props.actions.listEmployees);
+      } else {
+        employee.deleted = true;
+        this.props.actions
+          .removeEmployee(employee)
+          .then(this.props.actions.listEmployees);
+      }
+    }
+    render() {
+      const employee = this.props.employee;
+  
+      let rowClass = '';
+      if (employee.deleted) {
+        rowClass = 'faded';
+      }
+  
+      const button = (
+        <Button
+          onClick={() => {
+            this.handleClick(employee);
+          }}
+          bsStyle={employee.deleted ? 'success' : 'danger'}
+        >
+          {employee.deleted ? 'Restore' : 'Delete'}
+        </Button>
+      );
 
     return (
       <tr>
